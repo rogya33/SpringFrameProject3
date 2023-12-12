3806a7952add97dbbfba5ef21c4f2e9bafb5f92b
rogya33 <kei00216@gmail.com>
2023-12-12 15:59:50 +0900

    adjustment console log...master

--- src/proj2312121509/UserDao.java
+++ src/proj2312121509/UserDao.java
@@ -14,7 +14,7 @@ public class UserDao {
 		// TODO Auto-generated method stub
 		System.out.println("made by iss53");
 		
-		System.out.print("hello world");
+		System.out.print("hello master");
 	}
 
 }

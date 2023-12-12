86dc796f7e1069695c9bcacfa544dc7999f3a002
rogya33 <kei00216@gmail.com>
2023-12-12 15:48:35 +0900

    add hello method at iss53

--- src/proj2312121509/UserDao.java
+++ src/proj2312121509/UserDao.java
@@ -1,6 +1,9 @@
 package proj2312121509;
 
 public class UserDao {
+	public static void hello() {
+		System.out.println("hello method");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

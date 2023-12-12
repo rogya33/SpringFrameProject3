b6e46b45af7488291ad743feca16c8dec8ba3b42
rogya33 <kei00216@gmail.com>
2023-12-12 15:46:40 +0900

    modification main method at master

--- src/proj2312121509/UserDao.java
+++ src/proj2312121509/UserDao.java
@@ -5,6 +5,8 @@ public class UserDao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("made by iss53");
+		
+		System.out.print("hello world");
 	}
 
 }
